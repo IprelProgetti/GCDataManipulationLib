e8ae78cb8a18df9d48ce352e44a09dc196005ef5
Gabriele.Corni <gabriele_corni@iprel.it>
2019-02-27 17:06:32 +0100

    added setuptools support

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # GCDataManipulationLib
 Data manipulation library to easily move from/to narrow/wide table representations. 
+
+## Usage
+
+### Install
+
+Create your python virtual environment, activate it, then type:
+
+```
+git clone https://github.com/horns-g/GCDataManipulationLib.git
+cd GCDataManipulationLib
+python setup.py install
+```
+
+### Use
+
+Import gcornilib from within your python file and use it:
+
+```
+from gcornilib import DataManipulationV2 as dm2
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcornilib-horns-g",
+    version="0.0.1",
+    author="Gabriele Corni",
+    author_email="gabriele_corni@iprel.it",
+    description="Data manipulation pipeline for Machine Learning datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/horns-g/GCDataManipulationLib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+# python setup.py install
